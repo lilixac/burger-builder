ab13bf3a3163c45ca881dd831857c8ff7c7dce9f
lilixac <hinewton25@gmail.com>
2020-05-05 16:16:53 +0545

    Updates

---
+++ precache-manifest.1f3e7b43c71c9e7878b69b43731feb4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d2bbab03b2a148dcafddcf52fd0f5d",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "70591a2c26deaf284cc9",
+    "url": "/burger-builder/static/css/main.0c366552.chunk.css"
+  },
+  {
+    "revision": "35f8d6bd741668c182f4",
+    "url": "/burger-builder/static/js/2.69eb01ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-builder/static/js/2.69eb01ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70591a2c26deaf284cc9",
+    "url": "/burger-builder/static/js/main.fd59623c.chunk.js"
+  },
+  {
+    "revision": "d7e78e4710d08e6195c0",
+    "url": "/burger-builder/static/js/runtime-main.cb146d40.js"
+  }
+]);
